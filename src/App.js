a72499cae6604c7a2088d8d0dfe1b95a054088c8
Turgay Levent <turgayleventt@gmail.com>
2022-10-12 13:33:33 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.scss";
+import Header from "./component/Header/Header";
 import Navbar from "./component/Navbar/Navbar";
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
+      <Header />
     </div>
   );
 };
